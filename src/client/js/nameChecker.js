8de351af097b2d63edc38b83fbfc9de7b396a638
Natallia Markevich <natallia.markevich@gmail.com>
2021-07-02 21:32:54 -0500

    feat: Making validations user friendly

--- src/client/js/nameChecker.js
+++ src/client/js/nameChecker.js
@@ -2,14 +2,22 @@ function checkLink(inputText) {
     console.log("::: Running checkLink :::", inputText);
     const expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
     const regex = new RegExp(expression);
+    const validationText = document.getElementById("validationMsg");
+    const validationField = document.getElementById("linkFromUser");
 
     if (inputText == "") {
-        alert("The link is required");
+        validationText.innerHTML = "The link is required";
+        validationField.focus();
+        validationField.style.borderColor = "red";
+        validationText.style.visibility = "visible";
     } else if (inputText.match(regex)) {
+        validationText.style.visibility = "hidden";
         alert("Successful match");
     } else {
-        alert("No match");
-        
+        validationText.innerHTML = "The link is incorrect. Put the correct link";
+        validationField.focus();
+        validationField.style.borderColor = "red";
+        validationText.style.visibility = "visible";
     }
 }
 
