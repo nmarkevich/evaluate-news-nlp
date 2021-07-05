d17312dace6c69b8c0b77f0878a32ac862c4749f
Natallia Markevich <natallia.markevich@gmail.com>
2021-07-05 15:05:49 -0500

    feat: Adding listeners for the menu items

--- src/client/index.js
+++ src/client/index.js
@@ -9,9 +9,21 @@ import { handleSubmit } from './js/formHandler'
 export { checkLink, handleSubmit }
 
 const sectionItems = document.querySelectorAll("section");
+const menuItemLink = document.getElementById("toLink");
+const menuItemText = document.getElementById("toText");
+const sectionLink = document.getElementById("sectionLink");
+const sectionText = document.getElementById("sectionText");
 
-for (const menuItem of sectionItems) {
-    menuItem.addEventListener("click" , ( ) => {
-        menuItem.classList.toggle("collapsible");
+for (const sectionItem of sectionItems) {
+    sectionItem.addEventListener("click" , ( ) => {
+        sectionItem.classList.toggle("collapsible");
     });
 }
+
+menuItemLink.addEventListener("click" , ( ) => {
+    sectionLink.classList.add("collapsible");
+});
+
+menuItemText.addEventListener("click" , ( ) => {
+    sectionText.classList.add("collapsible");
+});
