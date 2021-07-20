64e87b0bd305881424e5de55501ddfe274f32b60
Natallia Markevich <natallia.markevich@gmail.com>
2021-07-20 11:56:38 -0500

    chore: Adding one more test for API call

--- __test__/mockAPI.test.js
+++ __test__/mockAPI.test.js
@@ -1,10 +1,18 @@
 import 'regenerator-runtime/runtime';
-import analyseDataLink from '../src/server/mockAPI.js';
+import { analyseDataText, analyseDataLink } from '../src/server/mockAPI.js';
 
 describe ("Testing the API calls", () => {
     test ("Testing API call using link", done => {
         try {
             expect(analyseDataLink).toBeDefined();
+            done();
+        } catch (error){
+            done(error);
+        }
+    }), 
+    test ("Testing API call using text", done => {
+        try {
+            expect(analyseDataText).toBeDefined();
             done();
         } catch (error){
             done(error);
