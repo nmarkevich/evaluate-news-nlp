ed77772f782797ef02001c418790e30828297cb9
Natallia Markevich <natallia.markevich@gmail.com>
2021-07-20 10:16:41 -0500

    chore: Adding more Jest tests for inputChecker

--- __test__/inputChecker.test.js
+++ __test__/inputChecker.test.js
@@ -1,11 +1,28 @@
 import { checkLink, checkText } from "../src/client/js/inputChecker.js";
 
 describe ("Testing the checkLink function", () => {
-    test("Testing empty input from a user", () => {
+    test("Testing empty input for checkLink function", () => {
         document.body.innerHTML = 
         '<p id="LinkValidationMsg">' + '</p>' + '<input id="linkFromUser">';
         const testLink = "";
         const ValidationMsg = document.getElementById("LinkValidationMsg");
         expect(checkLink(testLink)).toBeFalsy();
+    }),
+    test("Testing incorrect input for checkLink function", () => {
+        document.body.innerHTML = 
+        '<p id="LinkValidationMsg">' + '</p>' + '<input id="linkFromUser">';
+        const testLink = "google.com";
+        const ValidationMsg = document.getElementById("LinkValidationMsg");
+        expect(checkLink(testLink)).toBeFalsy();
+    })
+});
+
+describe ("Testing the checkText function", () => {
+    test("Testing empty input for checkText function", () => {
+        document.body.innerHTML = 
+        '<p id="textValidationMsg">' + '</p>' + '<input id="textFromUser">';
+        const testText = "";
+        const ValidationMsg = document.getElementById("textValidationMsg");
+        expect(checkText(testText)).toBeFalsy();
     })
 });
